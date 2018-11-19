b00b032394d0bb46473541104fb5e036dd20b432
Aimarekin <38393797+Aimarekin@users.noreply.github.com>
2018-11-19 19:01:14 +0100

    Create botconfig.py

---
+++ Bot/botconfig.py
@@ -0,0 +1,15 @@
+def token():
+    return "your_discord_token"
+    # Return the bot's token
+
+def admins():
+    return ["admin", "array", "IDs", "int"]
+    # Return a list of the IDs of users who should can run bot administration commands
+
+def main_server():
+    return "main_server_int"
+    # Return the ID of the main server, that BloxAd is based on.
+
+def manager_role():
+    return "manager_role_int"
+    # Return the ID of the manager role. This role must be in main_server().
